ee6399f4fabc35ed6b72d2d60ed18a4b90349b6a
Kanjira Anthony <149038851+AK47GT18@users.noreply.github.com>
2025-12-17 10:11:55 +0200

    Update main.py update to the cors

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://10.124.17.154:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://crop-advisory-delta.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
